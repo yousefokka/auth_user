1961acf7ea494f3b1dc8a992646f850506436f81
yousefokka <yousefokka2231@gmail.com>
2020-05-05 03:13:57 +0200

    some code edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   
     def create
       @user = User.new(user_params) 
+     
       if @user.save
         render json: @user, status: 201
       else
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 #require 'api_constraints.rb'
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  # resources :users, :only => [:show, :create, :update, :destroy]
   #resources :sessions, :only => [:create, :destroy]
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
       # We are going to list our resources here
       resources :users, :only => [:show, :create, :update, :destroy] 
       resources :ideas
-     resources :jobs
+      resources :jobs
       #end
       resources :sessions, :only => [:create, :destroy]
    # end

923114558430e34ecc0d650c4080423c06cc1dd0
yousefokka <yousefokka2231@gmail.com>
2020-05-07 13:23:51 +0200

    second commit

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -7,8 +7,8 @@ class Api::V1::SessionsController < ApplicationController
       user_email = params[:session][:email]
       @user = user_email.present? && User.find_by(email: user_email) 
   
-      if user.valid_password? user_password
-        sign_in user, store: false
+      if @user.valid_password? user_password
+        sign_in @user, store: false
         @user.generate_authentication_token!
         @user.save
         render json: @user, status: 200
--- config/routes.rb
+++ config/routes.rb
@@ -12,12 +12,12 @@ Rails.application.routes.draw do
       namespace :api do
         namespace :v1 do
       resources :users, :only => [:show, :create, :update, :destroy] 
+      resources :sessions, :only => [:create, :destroy]
          resources :ideas
          resources :jobs
       
       end
     end
-      resources :sessions, :only => [:create, :destroy]
    # end
   #end
 end
